c1c83b6b5ab415d5b527c6d081a91b0031a7fee8
= <=>
2021-02-19 20:30:52 +0300

    download add

--- src/main/java/org/example/web/controllers/BookShelfController.java
+++ src/main/java/org/example/web/controllers/BookShelfController.java
@@ -1,6 +1,8 @@
 package org.example.web.controllers;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.example.app.exceptions.FileUploadException;
 import org.example.app.services.BookService;
 import org.example.web.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +10,14 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -21,6 +26,7 @@ import javax.validation.Valid;
 public class BookShelfController {
     private Logger logger = Logger.getLogger(BookShelfController.class);
     private BookService bookService;
+    private List<File> fileList = new ArrayList<>();
 
     @Autowired
     public BookShelfController(BookService bookService) {
@@ -36,6 +42,7 @@ public class BookShelfController {
         model.addAttribute("bookAuthorToRemove", new BookAuthorToRemove());
         model.addAttribute("bookTitleToRemove", new BookTitleToRemove());
         model.addAttribute("bookList", bookService.getAllBooks());
+        model.addAttribute("fileList", fileList);
         return "book_shelf";
     }
 
@@ -130,5 +137,52 @@ public class BookShelfController {
             bookService.removeBookByTitle(bookTitleToRemove.getTitle());
             return "redirect:/books/shelf";
         }
+    }
+
+
+
+    @PostMapping("/uploadFile")
+    public String uploadFile(@RequestParam("file") MultipartFile file) throws Exception {
+        String name = file.getOriginalFilename();
+        byte[] bytes = file.getBytes();
+
+        //create dir
+        String rootPath = System.getProperty("catalina.home");
+        File dir = new File(rootPath + File.separator + "external_uploads");
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        //create file
+        File serverFile = new File(dir.getAbsoluteFile() + File.separator + name);
+
+        if (!file.isEmpty()) {
+            try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile))) {
+                stream.write(bytes);
+                fileList.add(serverFile);
+                logger.info("new file saved at: " + serverFile.getAbsolutePath());
+            }
+        } else {
+            logger.info("file upload fail");
+            throw new FileUploadException("file not found");
+        }
+        return "redirect:/books/shelf";
+    }
+
+    @GetMapping("/files/{file_name}")
+    public void DownloadFile(@RequestParam(value = "file_name") String fileName, HttpServletResponse response) {
+        try (BufferedInputStream stream = new BufferedInputStream(new FileInputStream(new File(fileName)))) {
+            IOUtils.copy(stream, response.getOutputStream());
+            response.flushBuffer();
+        } catch (IOException exception) {
+            logger.info("file download fail");
+            throw new RuntimeException("IOError writing file to output stream");
+        }
+    }
+
+    @ExceptionHandler(FileUploadException.class)
+    public String handlerError(Model model, FileUploadException exception) {
+        model.addAttribute("errorMessage", exception.getMessage());
+        return "errors/500";
     }
 }
